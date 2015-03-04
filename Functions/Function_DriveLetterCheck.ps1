f60a905afa007b6087db829a47054eae7676bda2
Daniel Lindegaard <magmadk@gmail.com>
2015-03-04 17:27:31 +0100

    1'st edit
    1'st upload

---
+++ Functions/Function_DriveLetterCheck.ps1
@@ -0,0 +1,24 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2015 v4.2.80
+	 Created on:   	04-03-2015 17:17
+	 Created by:   	Daniel Lindegaard
+	 Licence:	 	GNU licence - http://www.gnu.org/licenses/licenses.html
+	 Filename:     	Function_DriveLetterCheck.ps1
+	===========================================================================
+	.DESCRIPTION
+		Check if a driver letter is there or not and will return a $true or $false.
+		You can put as meney drives letters you like in -Driveletter as like, as long its comma(,) separated.
+#>
+
+function DriveLetterCheck ($DriveLetter)
+{ 
+	foreach ($Letter in $DriveLetter)
+	{
+		$DriveLetterCheck = Get-PSDrive | Where-Object -FilterScript { $_.Name -eq "$Letter" }
+		$DriveLetterCheck -eq $null
+	}
+}
+
+DriveLetterCheck -DriveLetter r,c
