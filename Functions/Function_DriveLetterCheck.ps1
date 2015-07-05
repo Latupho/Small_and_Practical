fbee76f23205d99ac54e58f93bb038620c28a727
Daniel Lindegaard - Work <magmadk@gmail.com>
2015-07-05 15:24:06 +0200

    Test
    This is a test to see if I can Push a new Commit to Github, from this
    new Laptop i got.

--- Functions/Function_DriveLetterCheck.ps1
+++ Functions/Function_DriveLetterCheck.ps1
@@ -4,13 +4,13 @@
 	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2015 v4.2.80
 	 Created on:   	04-03-2015 17:17
 	 Created by:   	Daniel Lindegaard
-	 Source:	https://github.com/Latupho/Small_and_Practical/blob/master/Functions/Function_DriveLetterCheck.ps1
-	 Licence:	GNU licence - http://www.gnu.org/licenses/licenses.html
+	 Source:	    https://github.com/Latupho/Small_and_Practical/blob/master/Functions/Function_DriveLetterCheck.ps1
+	 Licence:	    GNU licence - http://www.gnu.org/licenses/licenses.html
 	 Filename:     	Function_DriveLetterCheck.ps1
 	===========================================================================
 	.DESCRIPTION
 		Check if a driver letter is there or not and will return a $true or $false.
-		You can put as meney drives letters you like in -Driveletter as like, as long its comma(,) separated.
+		You can put as meney drives letters you like in -Driveletter as like, as long its comma(,) separated...
 #>
 
 function DriveLetterCheck ($DriveLetter)
