d0430ec0829a8bdf3642b05671936c46bf2b43ad
Daniel Lindegaard - Work <magmadk@gmail.com>
2015-11-18 12:30:03 +0100

    First upload.
    I have run some test, and as fare I can see, it will gather all the
    Pathes that there is installed, and you can find under "View Installed
    Updates" under "add or remove programs" under the controle panal.
    
    The ourput, is all information and in 2 types or object format.

---
+++ Functions/Get-MSPatch.ps1
@@ -0,0 +1,51 @@
+﻿
+function Get-MSPatch ($KB,$Quiet) {
+    $Quiet = $false
+    #$KBs are for when I need to ask WMI(Win32_QuickFixEngineering/Get-hotfix) for the KB's.
+    $Count = 0
+    $QueryStringAssembly = ""
+
+    #This part is to create there FilterScript String, we need to search for KB patches in the Registry.
+    $FilterScript = @() 
+    $KBObj = $kb -split ","
+    $KBObjwStart = $KBObj | ForEach-Object -Process { '$_.displayname -like ' + ('"*' + $_ + '*"')}
+    $WhereString = $KBObjwStart -join " -or "
+    $FilterScript = [scriptblock]::Create( $WhereString )
+
+    #Search the Reg.
+    $Reg = @("HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*")
+    $InstalledApps = Get-ItemProperty $Reg -EA 0
+    $Run = 0
+
+    $OfficeKBs = $InstalledApps |
+    Where-Object -FilterScript {
+        ($FilterScript.invoke())
+    }
+
+    foreach ($Item in $KBObj) {
+        $QueryStringAssembly += if ($Count -eq '0') {"where HotFixID='$Item'"} else { " OR HotFixID='$Item'"}
+        $Count++
+    }
+    $WMIQuery =  "select * from Win32_QuickFixEngineering " + "$QueryStringAssembly"
+
+    $WinKBs = Get-WmiObject -Query $WMIQuery
+
+#########################################################################################################################
+
+    if ($Quiet)  {
+        If ($OfficeKBs -eq $null -and $WinKBs -eq $null) {
+            Write-Output $false
+        }
+        else {
+            Write-Output $true
+        }
+    }
+    Else {
+            Write-Host "Microsoft Hotfixs Patches"
+            Write-Output $WinKBs
+            Write-host "Microsoft Office Patches"
+            Write-Output $OfficeKBs
+    }
+}
+
+Get-MSPatch -KB "KB3054886,KB3055034,KB3101521,KB3097877,KB3074230,KB3097996,KB3024995" -Quiet $false
