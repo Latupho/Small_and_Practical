b7b9a8587336c45dec313db25a30ae84f0e25d0d
Daniel Lindegaard - Work <magmadk@gmail.com>
2015-11-19 09:43:42 +0100

    Outbut to Objects
    And now it will output a custom Object, ensted of 2 types of Objects.
    The orginal Object is still in the output, under the property
    "FullObject". Do a $Obj.FullObject and you will get the data.

--- Functions/Get-MSPatch.ps1
+++ Functions/Get-MSPatch.ps1
@@ -20,15 +20,18 @@ function Get-MSPatch ($KB,$Quiet) {
     $OfficeKBs = $InstalledApps |
     Where-Object -FilterScript {
         ($FilterScript.invoke())
-    }
+    } |
+	Add-Member -MemberType NoteProperty -Name 'Original Source' -Value 'Registry Database' -PassThru
 
     foreach ($Item in $KBObj) {
         $QueryStringAssembly += if ($Count -eq '0') {"where HotFixID='$Item'"} else { " OR HotFixID='$Item'"}
         $Count++
     }
     $WMIQuery =  "select * from Win32_QuickFixEngineering " + "$QueryStringAssembly"
-
-    $WinKBs = Get-WmiObject -Query $WMIQuery
+	
+	$WinKBs = Get-WmiObject -Query $WMIQuery |
+	Select-Object -Property * |
+	Add-Member -MemberType NoteProperty -Name 'Original Source' -Value 'Win32_QuickFixEngineering' -PassThru
 
 #########################################################################################################################
 
@@ -41,11 +44,35 @@ function Get-MSPatch ($KB,$Quiet) {
         }
     }
     Else {
-            Write-Host "Microsoft Hotfixs Patches"
-            Write-Output $WinKBs
-            Write-host "Microsoft Office Patches"
-            Write-Output $OfficeKBs
-    }
+		$ObjKB = @()
+		$ObjKB += $WinKBs
+		$ObjKB += $OfficeKBs
+				
+			foreach ($KBPatch in $ObjKB) {
+			if ($KBPatch.'Original Source' -eq "Win32_QuickFixEngineering") {
+				$object = [pscustomobject]@{
+					DisplayName = 'N\A';
+					KBNumber = $KBPatch.HotFixID;
+					InstalledOn = $KBPatch.InstalledOn;
+					UninstallString = 'N\A';
+					Description = $KBPatch.Description;
+					FullObject = $KBPatch #This all the will Object, if it's needed.
+				}
+			}
+			elseif ($KBPatch.'Original Source' -eq "Registry Database") {
+				$object = [pscustomobject]@{
+					DisplayName = $KBPatch.DisplayName;
+					KBNumber = $KBPatch.'(default)';
+					InstalledOn = $KBPatch.InstalledOn;
+					UninstallString = $KBPatch.UninstallString;
+					Description = $KBPatch.Comments;
+					FullObject = $KBPatch #This all the will Object, if it's needed.
+				}
+			}
+			Write-Output $object
+			}
+		
+		}
 }
 
 Get-MSPatch -KB "KB3054886,KB3055034,KB3101521,KB3097877,KB3074230,KB3097996,KB3024995" -Quiet $false
